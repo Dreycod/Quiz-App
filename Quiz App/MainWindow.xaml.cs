635cd1aa3ae25dcc0be1435f527223f4855da315
Akiel <122918848+Akieldamas@users.noreply.github.com>
2023-12-21 16:26:57 +0100

    Test Phase 2.0

--- Quiz App/MainWindow.xaml.cs
+++ Quiz App/MainWindow.xaml.cs
@@ -27,8 +27,38 @@ namespace Quiz_App
     {
         int Amount;
         int Category;
+        string StringCategory;
         string Difficulty;
         string Type;
+
+        // Create me a dictionnary of string and int 
+        Dictionary<string, int> categories = new Dictionary<string, int>()
+        {
+            {"General Knowledge", 9},
+            {"Books", 10},
+            {"Film", 11},
+            {"Music", 12},
+            {"Musicals & Theatres", 13},
+            {"Television", 14},
+            {"Video Games", 15},
+            {"Board Games", 16},
+            {"Science & Nature", 17},
+            {"Computers", 18},
+            {"Mathematics", 19},
+            {"Mythology", 20},
+            {"Sports", 21},
+            {"Geography", 22},
+            {"History", 23},
+            {"Politics", 24},
+            {"Art", 25},
+            {"Celebrities", 26},
+            {"Animals", 27},
+            {"Vehicles", 28},
+            {"Comics", 29},
+            {"Gadgets", 30},
+            {"Anime & Manga", 31},
+            {"Cartoon & Animations", 32}
+        };
 
         QuizAPI quizAPI;
 
@@ -42,10 +72,11 @@ namespace Quiz_App
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Amount = int.Parse(TB_Amount.Text);
-            Category = int.Parse(TB_Category.Text);
+            StringCategory = TB_Category.Text;
             Difficulty = TB_Difficulty.Text;
             Type = TB_Type.Text;
 
+            Category = categories[StringCategory];
             GetQuiz();
         }
 
@@ -61,13 +92,29 @@ namespace Quiz_App
                 Debug.WriteLine("Error in getting data");
                 return;
             }
-            MessageBox.Show(root.results[0].question.ToString());
+            QuestionLabel.Content = root.results[0].question.ToString();
+
+            if (root.results[0].type == "multiple")
+            {
+                Answer1Button.Content = root.results[0].correct_answer.ToString();
+                Answer2Button.Content = root.results[0].incorrect_answers[0].ToString();
+                Answer3Button.Content = root.results[0].incorrect_answers[1].ToString();
+                Answer4Button.Content = root.results[0].incorrect_answers[2].ToString();
+            }
+            
+
+            
             
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void ConfirmAnswersButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
