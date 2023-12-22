5b6856cab766f0c5d066311bc00c449a05672d28
Az3r1y <137179080+Az3r1y@users.noreply.github.com>
2023-12-22 16:28:57 +0100

    Classes and beginning of systems

--- Quiz App/MainWindow.xaml.cs
+++ Quiz App/MainWindow.xaml.cs
@@ -32,52 +32,25 @@ namespace Quiz_App
         string Type;
 
         // Create me a dictionnary of string and int 
-        Dictionary<string, int> categories = new Dictionary<string, int>()
-        {
-            {"General Knowledge", 9},
-            {"Books", 10},
-            {"Film", 11},
-            {"Music", 12},
-            {"Musicals & Theatres", 13},
-            {"Television", 14},
-            {"Video Games", 15},
-            {"Board Games", 16},
-            {"Science & Nature", 17},
-            {"Computers", 18},
-            {"Mathematics", 19},
-            {"Mythology", 20},
-            {"Sports", 21},
-            {"Geography", 22},
-            {"History", 23},
-            {"Politics", 24},
-            {"Art", 25},
-            {"Celebrities", 26},
-            {"Animals", 27},
-            {"Vehicles", 28},
-            {"Comics", 29},
-            {"Gadgets", 30},
-            {"Anime & Manga", 31},
-            {"Cartoon & Animations", 32}
-        };
+        
 
         QuizAPI quizAPI;
+        Category category;
 
         public MainWindow()
         {
             InitializeComponent();
 
             quizAPI = new QuizAPI();
+            category = new Category("");
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CategoryBTN_Click(object sender, RoutedEventArgs e)
         {
-            Amount = int.Parse(TB_Amount.Text);
-            StringCategory = TB_Category.Text;
-            Difficulty = TB_Difficulty.Text;
-            Type = TB_Type.Text;
-
-            Category = categories[StringCategory];
-            GetQuiz();
+            Button btn = (Button) sender;
+            string content = btn.Content.ToString();
+            
+            category = new Category(content);
         }
 
         private void TopBar_MouseDown(object sender, MouseButtonEventArgs e)
@@ -92,19 +65,45 @@ namespace Quiz_App
                 Debug.WriteLine("Error in getting data");
                 return;
             }
-            QuestionLabel.Content = root.results[0].question.ToString();
+            QuestionLabel.Text = root.results[0].question.ToString();
 
             if (root.results[0].type == "multiple")
             {
-                Answer1Button.Content = root.results[0].correct_answer.ToString();
-                Answer2Button.Content = root.results[0].incorrect_answers[0].ToString();
-                Answer3Button.Content = root.results[0].incorrect_answers[1].ToString();
-                Answer4Button.Content = root.results[0].incorrect_answers[2].ToString();
-            }
-            
+                // Randomize the answers
+                Random rnd = new Random();
+                int random = rnd.Next(0, 4);
 
-            
-            
+                
+
+                if (random == 0)
+                {
+                    Answer1Button.Content = root.results[0].correct_answer.ToString();
+                    Answer2Button.Content = root.results[0].incorrect_answers[0].ToString();
+                    Answer3Button.Content = root.results[0].incorrect_answers[1].ToString();
+                    Answer4Button.Content = root.results[0].incorrect_answers[2].ToString();
+                }
+                else if (random == 1)
+                {
+                    Answer1Button.Content = root.results[0].incorrect_answers[0].ToString();
+                    Answer2Button.Content = root.results[0].correct_answer.ToString();
+                    Answer3Button.Content = root.results[0].incorrect_answers[1].ToString();
+                    Answer4Button.Content = root.results[0].incorrect_answers[2].ToString();
+                }
+                else if (random == 2)
+                {
+                    Answer1Button.Content = root.results[0].incorrect_answers[0].ToString();
+                    Answer2Button.Content = root.results[0].incorrect_answers[1].ToString();
+                    Answer3Button.Content = root.results[0].correct_answer.ToString();
+                    Answer4Button.Content = root.results[0].incorrect_answers[2].ToString();
+                }
+                else if (random == 3)
+                {
+                    Answer1Button.Content = root.results[0].incorrect_answers[0].ToString();
+                    Answer2Button.Content = root.results[0].incorrect_answers[1].ToString();
+                    Answer3Button.Content = root.results[0].incorrect_answers[2].ToString();
+                    Answer4Button.Content = root.results[0].correct_answer.ToString();
+                }   
+            }    
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
