f9f5c31c41646067a3b04bdea51a376f594bebaf
Drey <84332780+Dreycod@users.noreply.github.com>
2024-01-11 17:20:58 +0100

    Achievement

--- Quiz App/Model/Achievement.cs
+++ Quiz App/Model/Achievement.cs
@@ -8,10 +8,10 @@ namespace Quiz_App.Model
 {
     public class Achievement
     {
-        //    public string AchievementName { get; set; }
-        //    public bool isAchievementUnlocked { get; set; }
+        public string Name { get; set; }
+        public bool isAchievementUnlocked { get; set; }
+        public int Requirements { get; set; }
         public string ImageUrl { get; set; }
         public string Content { get; set; }
-        //    public int AchievementRequirements { get; set; }
     }
 }
